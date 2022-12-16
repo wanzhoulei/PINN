0d1838a590c3be5ea4400c59ac524d54ce2c3de8
wal076 <wanzhoulei@harvard.edu>
2022-12-16 16:26:10 -0500

    add GaussNewton 1d Poisson Exp

---
+++ GaussNewton_1dPoisson.py
@@ -0,0 +1,49 @@
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from PINN_tools import *
+import time
+import scipy.optimize
+
+##change the number of iterations, damping coeff alpha and random seed here if needed
+n_iter = 7000; alpha=0
+random_seed = 20 #set the random seed for generating the initial parameters of PINN
+
+N_f = 1000 #Total number of collocation points 
+# Training data
+X_f_train, X_u_train, u_train = trainingdata(N_f, sample=False)
+layers = np.array([1,20, 20,1]) #2 hidden layers
+
+PINN = Sequentialmodel(layers, seed=random_seed)
+init_params = PINN.get_weights().numpy()
+
+##construct the l2 kernel without damping
+kernel = L2Kernel(PINN, alpha=alpha)
+
+s = time.time()
+results = scipy.optimize.minimize(fun = PINN.optimizerfunc, 
+                                  x0 = init_params, 
+                                  args=(), 
+                                  method='Newton-CG', 
+                                  jac= True,        # If jac is True, fun is assumed to return the gradient along with the objective function
+                                  hess = kernel,
+                                  callback = PINN.optimizer_callback, 
+                                  options = {'disp': None,
+                                             'maxiter': n_iter, 
+                                             'xtol': 1e-20
+                                            })
+e = time.time()
+print("Entire time: {}".format(e-s))
+print("CPU time each iteration: {}".format((e-s)/2000))
+
+#build the result folder if not exists
+mydir = ("results")
+if not os.path.isdir(mydir):
+    os.makedirs(mydir)
+##save the resulting convergence trace
+tracepath = "./results/GN_{}iter_seed{}_alpha{}.csv".format(n_iter, random_seed, alpha)
+np.savetxt(tracepath, np.array(PINN.loss_trace), delimiter=",")
+
+##save the plot
+figpath = './results/GN_{}iter_seed{}_alpha{}_plot.png'.format(n_iter, random_seed, alpha)
+plot_nn(PINN.loss_trace, PINN, lr=None, figpath=figpath)
