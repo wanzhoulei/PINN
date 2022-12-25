e51ccccaad1c1c4c9148798764597c362404c194
wal076 <wanzhoulei@harvard.edu>
2022-12-25 13:56:31 -0500

    add gamma weight and cpu time to Hinv NGD 2d

--- Hinv_2dPoisson.py
+++ Hinv_2dPoisson.py
@@ -13,19 +13,20 @@ import time
 import scipy.optimize
 
 ##change the number of iterations, damping coeff alpha and random seed here if needed
-n_iter = 1000; alpha=0
+n_iter = 2000; alpha=0; gamma=1.99
 random_seed = 0 #set the random seed for generating the initial parameters of PINN
 
 N = 50 ##use 50 by 50 grid points
-layers = np.array([2, 30, 30, 1]) #2 hidden layers
+layers = np.array([2, 20, 30, 20, 1]) #3 hidden layers
 X_f_train, X_u_train, u_train = gridData(N)
 
-PINN = Sequentialmodel(layers, seed=random_seed, N=N)
+PINN = Sequentialmodel(layers, seed=random_seed, N=N, gamma=gamma)
 init_params = PINN.get_weights().numpy()
 kernel = HinvKernel(PINN, N, X_f_train, alpha=alpha)
 
 # train the model with Scipy newton-cg optimizer to run L2 GD
 s = time.time()
+PINN.start_time = s
 results = scipy.optimize.minimize(fun = PINN.optimizerfunc, 
                                   x0 = init_params, 
                                   args=(), 
@@ -46,27 +47,31 @@ mydir = ("results")
 if not os.path.isdir(mydir):
     os.makedirs(mydir)
 ##save the resulting convergence trace
-tracepath = "./results/2dpoisson_Hinv_{}iter_seed{}_alpha{}_f_{}_N40_{}.csv".format(n_iter, 
-    random_seed, str(alpha).replace('.','d'), str(func_RHS).split(' ')[1], layertostr(layers))
+tracepath = "./results/2dpoisson_Hinv_{}iter_seed{}_alpha{}_f_{}_N{}_{}_gamma{}.csv".format(n_iter, 
+    random_seed, str(alpha).replace('.','d'), str(func_RHS).split(' ')[1], N, layertostr(layers), gamma)
 np.savetxt(tracepath, np.array(PINN.loss_trace), delimiter=",")
+
+#save the cpu time
+tracepath2 = "./results/2dpoisson_Hinv_{}iter_seed{}_alpha{}_f_{}_N{}_{}_gamma{}_CPUtime.csv".format(n_iter, 
+    random_seed, str(alpha).replace('.','d'), str(func_RHS).split(' ')[1], N, layertostr(layers), gamma)
+np.savetxt(tracepath2, np.array(PINN.clock), delimiter=",")
 
 ##save the plot
 PINN.set_weights(results.x)
 u_pred = PINN.evaluate(X_u_test)
 u_pred = np.reshape(u_pred,(256,256),order='F') 
-figpath = './results/2dpoisson_Hinv_{}iter_seed{}_alpha{}_f_{}_N40_{}_solution.png'.format(n_iter, 
-    random_seed, str(alpha).replace('.','d'), str(func_RHS).split(' ')[1], layertostr(layers))
+figpath = './results/2dpoisson_Hinv_{}iter_seed{}_alpha{}_f_{}_N{}_{}_gamma{}_solution.png'.format(n_iter, 
+    random_seed, str(alpha).replace('.','d'), str(func_RHS).split(' ')[1], N, layertostr(layers), gamma)
 solutionplot(u_pred, usol, figpath)
-plt.show()
+plt.clf()
 plt.cla()
 
-
+plt.figure(figsize=(8, 6))
 plt.plot(PINN.loss_trace)
 plt.xlabel("number of iteration")
 plt.ylabel("loss function")
 plt.title("H-1 NGD on 2d Poisson with PINN loss trace")
-plt.show()
 ##save the convergence trace plot
-pltpath = "./results/2dpoisson_Hinv_{}iter_seed{}_alpha{}_f_{}_N40_{}_loss.png".format(n_iter, 
-    random_seed, str(alpha).replace('.','d'), str(func_RHS).split(' ')[1], layertostr(layers))
+pltpath = "./results/2dpoisson_Hinv_{}iter_seed{}_alpha{}_f_{}_N{}_{}_loss_gamma{}.png".format(n_iter, 
+    random_seed, str(alpha).replace('.','d'), str(func_RHS).split(' ')[1], N, layertostr(layers), gamma)
 plt.savefig(pltpath, dpi=300)
