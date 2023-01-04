0072d2778b2a8ebae16186ed81f2e5e882eebc8c
wal076 <wanzhoulei@harvard.edu>
2023-01-04 00:31:15 -0500

    add pickle feature

--- BFGS_2dPoisson.py
+++ BFGS_2dPoisson.py
@@ -5,6 +5,7 @@ It solves the outputs in the results folder
 
 '''
 
+import pickle
 import os
 import numpy as np
 from matplotlib import pyplot as plt
@@ -13,21 +14,32 @@ import time
 import scipy.optimize
 
 ##change the number of iterations, damping coeff alpha and random seed here if needed
-n_iter = 1000
+n_iter = 100; gamma = 1.99
+write_pickle = True ##whether to serialize the PINN object
+read_picke = None #provide the link to the .dat file to resume the GD
 random_seed = 0 #set the random seed for generating the initial parameters of PINN
 
 N = 50 ##use 40 by 40 grid points
-layers = np.array([2, 30, 30, 1]) #2 hidden layers
+layers = np.array([2, 20, 30, 20, 1]) #2 hidden layers
 X_f_train, X_u_train, u_train = gridData(N)
 
 maxcor = 200 ##maximum memory in L-BFGS 
 max_iter = 10000
 
-PINN = Sequentialmodel(layers, seed=random_seed, N=N)
+PINN = 0
+if read_picke is not None:
+    with open(read_picke, 'rb') as f:
+        PINN = pickle.load(f)
+else:
+    PINN = Sequentialmodel(layers, seed=random_seed, N=N, gamma=gamma)
 init_params = PINN.get_weights().numpy()
 
 # train the model with Scipy newton-cg optimizer to run L2 GD
 s = time.time()
+if read_picke is None:
+    PINN.start_time = s
+else:
+    PINN.start_time = s - PINN.clock[-1]
 # train the model with Scipy L-BFGS optimizer
 results = scipy.optimize.minimize(fun = PINN.optimizerfunc, 
                                   x0 = init_params, 
@@ -53,26 +65,32 @@ mydir = ("results")
 if not os.path.isdir(mydir):
     os.makedirs(mydir)
 ##save the resulting convergence trace
-tracepath = "./results/2dpoisson_BFGS_{}iter_seed{}_f_{}_N40_{}.csv".format(n_iter, 
-    random_seed, str(func_RHS).split(' ')[1], layertostr(layers))
+tracepath = "./results/2dpoisson_BFGS_{}iter_seed{}_f_{}_N{}_{}-gamma{}.csv".format(len(PINN.loss_trace), 
+    random_seed, str(func_RHS).split(' ')[1], N, layertostr(layers), gamma)
 np.savetxt(tracepath, np.array(PINN.loss_trace), delimiter=",")
 
 ##save the plot
 PINN.set_weights(results.x)
 u_pred = PINN.evaluate(X_u_test)
 u_pred = np.reshape(u_pred,(256,256),order='F') 
-figpath = './results/2dpoisson_BFGS_{}iter_seed{}_f_{}_N40_{}_solution.png'.format(n_iter, 
-    random_seed, str(func_RHS).split(' ')[1], layertostr(layers))
+figpath = './results/2dpoisson_BFGS_{}iter_seed{}_f_{}_N{}_{}_gamma{}_solution.png'.format(len(PINN.loss_trace), 
+    random_seed, str(func_RHS).split(' ')[1], N, layertostr(layers), gamma)
 solutionplot(u_pred, usol, figpath)
-plt.show()
+plt.clf()
 plt.cla()
 
 ##save the convergence trace plot
-pltpath = "./results/2dpoisson_BFGS_{}iter_seed{}_f_{}_N40_{}_loss.png".format(n_iter, 
-    random_seed, str(func_RHS).split(' ')[1], layertostr(layers))
+plt.figure(figsize=(8, 6))
+pltpath = "./results/2dpoisson_BFGS_{}iter_seed{}_f_{}_N{}_{}_gamma{}_loss.png".format(len(PINN.loss_trace), 
+    random_seed, str(func_RHS).split(' ')[1], N, layertostr(layers), gamma)
 plt.plot(PINN.loss_trace)
 plt.xlabel("number of iteration")
 plt.ylabel("loss function")
 plt.title("BFGS on 2d Poisson with PINN loss trace")
 plt.savefig(pltpath, dpi=300)
-plt.show()
+
+if write_pickle:
+    pickle_writepath = './results/2dpoisson_lbfgs_{}iter_seed{}_f_{}_N{}_{}_PINN_gamma{}.dat'.format(len(PINN.loss_trace), 
+        random_seed, str(func_RHS).split(' ')[1], N, layertostr(layers), gamma)
+    with open(pickle_writepath, 'wb') as f:
+        pickle.dump(PINN, f)
